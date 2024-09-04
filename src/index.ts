a7df71ceeb26e817b5ca5e362df3a97b103cc055
Taylor Smith <tsmith@tsmithcreative.com>
2024-09-04 10:39:57 -0500

    Fetch something we know, output as-is

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,15 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+// For now, let's just operate on a known short VTT
+const PLACEHOLDER_VTT = 'https://customer-igynxd2rwhmuoxw8.cloudflarestream.com/86319f6f2c108ce2487d2a9bb6be9234/text/en.vtt?p=eyJ0eXBlIjoiZmlsZSIsInZpZGVvSUQiOiI4NjMxOWY2ZjJjMTA4Y2UyNDg3ZDJhOWJiNmJlOTIzNCIsIm93bmVySUQiOjM0MjA5Mjc1LCJjcmVhdG9ySUQiOiJzdHJlYW0iLCJ0cmFjayI6ImY1NTIwYWRiNjNiYzY2Njg3ZGNkM2ViMDZmMjUxYzQ4IiwicmVuZGl0aW9uIjoiNzM4NDE1MjE5IiwibXV4aW5nIjoiNzkyOTc3MjI2In0&s=w7Y2YgLCkkwEwpNwCWIWwqM3TFfDmcKAwqIiWcKoWkrDvMKOwqhpNB3Ck2c';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env, ctx): Promise<Response> {
+		// Step One: Get our input. For now, use a known placeholder and skip error handling.
+		const input = await fetch(PLACEHOLDER_VTT).then(res => res.text());
+
+		// Done: Return what we have.
+		return new Response(input);
 	},
-} satisfies ExportedHandler<Env>;
+};
